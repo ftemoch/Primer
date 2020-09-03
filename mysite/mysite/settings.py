7b09fe6680d0461d305305deb3bb164a81f1f464
Fernando Temoch <ftemoche@hotmail.com>
2020-09-02 21:35:04 -0500

    Todo bien

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%7u(8+x*9+f8*0)4_zabd8nd!l8a@@8w8hfl38*pv$^%m_^^2@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['fernandotemoche-projecto1.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['fernandotemoche-clasefinal.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -76,13 +76,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
 """
 DATABASES = {
     'default': {
@@ -94,6 +96,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+"""
 
 
 # Password validation
